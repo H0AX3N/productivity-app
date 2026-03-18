0332387954778717855ce18645f15f939d3e16df
H0AX3N <sandipandebgithub@gmail.com>
2026-03-18 15:20:51 +0530

    Added theme switching feature

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { Moon, Sun } from "lucide-react";
+import Todo from "./components/Todo";
+import { useState } from "react";
+
+function App() {
+  const [theme, setTheme] = useState(true);
+  function handleThemeChange() {
+    const root = document.documentElement;
+    const isDark = root.classList.toggle("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+    setTheme(!theme);
+  }
+
+  return (
+    <div className='relative w-full h-full flex flex-col items-center bg-white dark:bg-black transition-colors duration-300'>
+      <Pattern />
+
+      <div className="fixed top-0 left-0 w-full h-32 z-11 pointer-events-none
+        backdrop-blur-md [mask-image:linear-gradient(to_bottom,black_20%,transparent_100%)]">
+      </div>
+
+      <div className="fixed bottom-0 left-0 w-full h-32 z-11 pointer-events-none
+        backdrop-blur-md [mask-image:linear-gradient(to_top,black_20%,transparent_100%)]">
+      </div>
+
+      <button 
+        onClick={handleThemeChange} 
+        className="fixed top-5 right-6 z-12 ml-4 rounded-xl p-4 bg-neutral-300 dark:bg-neutral-800 dark:text-white"
+      >
+        {theme ? <Sun/> : <Moon/>}
+      </button>
+
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
+}
+
+export default App
+
+function Pattern() {
+  return (
+    <div
+      className="absolute inset-0 z-0 bg-[image:repeating-linear-gradient(315deg,var(--color-pattern-fg)_0,var(--color-pattern-fg)_0.7px,transparent_0,transparent_50%)] bg-[size:10px_10px] bg-fixed transition-colors duration-300"
+    >
+    </div>
+  );
+}
---
+++ src/components/Todo.jsx
@@ -0,0 +1,49 @@
+import { Trash } from 'lucide-react'
+import React from 'react'
+
+function Todo() {
+    const todos = [
+        { "id": 1, "task": "Buy groceries", "completed": false, "priority": "High", "deleteIcon": <Trash /> },
+        { "id": 2, "task": "Finish report", "completed": true, "priority": "Medium", "deleteIcon": <Trash /> },
+        { "id": 3, "task": "Call Mom", "completed": true, "priority": "High", "deleteIcon": <Trash /> },
+        { "id": 4, "task": "Water plants", "completed": false, "priority": "Medium", "deleteIcon": <Trash /> },
+        { "id": 5, "task": "Schedule dentist", "completed": false, "priority": "Medium", "deleteIcon": <Trash /> },
+        { "id": 6, "task": "Read 20 pages", "completed": false, "priority": "Low", "deleteIcon": <Trash /> },
+        { "id": 7, "task": "Workout", "completed": true, "priority": "Low", "deleteIcon": <Trash /> },
+        { "id": 8, "task": "Clean kitchen", "completed": true, "priority": "Low", "deleteIcon": <Trash /> },
+        { "id": 9, "task": "Pay electric bill", "completed": true, "priority": "Low", "deleteIcon": <Trash /> },
+        { "id": 10, "task": "Update resume", "completed": false, "priority": "High", "deleteIcon": <Trash /> },
+        { "id": 11, "task": "Email team", "completed": true, "priority": "Medium", "deleteIcon": <Trash /> },
+        { "id": 12, "task": "Walk the dog", "completed": false, "priority": "High", "deleteIcon": <Trash /> },
+        { "id": 13, "task": "Prep lunch", "completed": false, "priority": "Medium", "deleteIcon": <Trash /> },
+        { "id": 14, "task": "Research API", "completed": true, "priority": "High", "deleteIcon": <Trash /> },
+        { "id": 15, "task": "Fix CSS bug", "completed": true, "priority": "Medium", "deleteIcon": <Trash /> },
+        { "id": 16, "task": "Journaling", "completed": false, "priority": "High", "deleteIcon": <Trash /> },
+        { "id": 17, "task": "Listen to podcast", "completed": false, "priority": "High", "deleteIcon": <Trash /> },
+        { "id": 18, "task": "Organize desk", "completed": false, "priority": "Low", "deleteIcon": <Trash /> },
+        { "id": 19, "task": "Check mail", "completed": true, "priority": "High", "deleteIcon": <Trash /> },
+        { "id": 20, "task": "Meditation", "completed": false, "priority": "Medium", "deleteIcon": <Trash /> },
+        { "id": 20, "task": "Meditation", "completed": false, "priority": "Medium", "deleteIcon": <Trash /> },
+        { "id": 20, "task": "Meditation", "completed": false, "priority": "Medium", "deleteIcon": <Trash /> },
+        { "id": 20, "task": "Meditation", "completed": false, "priority": "Medium", "deleteIcon": <Trash /> },
+    ]
+
+    return (
+        <div className='relative z-10 h-150 w-85 mt-20 p-5 flex flex-col gap-2 bg-white dark:bg-neutral-900 rounded-xl shadow-lg border-2 border-neutral-100 dark:border-neutral-800'>
+            <input type="text" className='border border-neutral-300 dark:border-neutral-800 dark:bg-black dark:text-white rounded-lg p-2' />
+            <div className='h-full w-full rounded-lg border border-neutral-300 dark:border-neutral-800 overflow-scroll sm:no-scrollbar'>
+                {todos.map((item, idx) => {
+                    return (
+                        <div className='flex justify-between items-center p-2'>
+                            <li className='dark:text-white' key={idx}>{item.task}</li>
+                            <button className='bg-red-500 p-1 rounded-md text-white' key={idx}>{item.deleteIcon}</button>
+                        </div>
+                    )
+                })}
+            </div>
+            <button className='text-white bg-black dark:bg-emerald-500 py-3 rounded-lg font-semibold'>Add Todo</button>
+        </div>
+    )
+}
+
+export default Todo
